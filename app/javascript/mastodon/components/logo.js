c8268e64fd4333a58ad127b7f3d77188e6f80696
skin-soc <82213990+skin-soc@users.noreply.github.com>
2023-01-21 19:23:11 +0100

    Update logo.js

--- app/javascript/mastodon/components/logo.js
+++ app/javascript/mastodon/components/logo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import LogoSvg from "/app/javascript/images/logo-symbol-wordmark.svg";
+import LogoSvg from "/app/javascript/images/preview.png";
 
 const Logo = () => (
   <img src={LogoSvg} alt="The Antisocial Network" />
