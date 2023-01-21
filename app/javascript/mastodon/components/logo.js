b113533a48485da5d4090af7ef66668147de6a99
skin-soc <82213990+skin-soc@users.noreply.github.com>
2023-01-21 17:22:34 +0100

    Update logo.js

--- app/javascript/mastodon/components/logo.js
+++ app/javascript/mastodon/components/logo.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import ImmutablePropTypes from 'react-immutable-proptypes';
-import { defineMessages, injectIntl, FormattedMessage } from 'react-intl';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import Column from 'mastodon/components/column';
-import LinkFooter from 'mastodon/features/ui/components/link_footer';
-import { Helmet } from 'react-helmet';
-import { fetchServer, fetchExtendedDescription, fetchDomainBlocks } from 'mastodon/actions/server';
-import Account from 'mastodon/containers/account_container';
-import Skeleton from 'mastodon/components/skeleton';
-import Icon from 'mastodon/components/icon';
-import classNames from 'classnames';
-import Image from 'mastodon/components/image';
 
 const Logo = () => (
-  <Image blurhash={server.getIn(['thumbnail', 'blurhash'])} src={server.getIn(['thumbnail', 'url'])} srcSet={server.getIn(['thumbnail', 'versions'])?.map((value, key) => `${value} ${key.replace('@', '')}`).join(', ')} className='about__header__hero' />
+  <svg viewBox='0 0 261 66' className='logo' role='img'>
+    <title>The Antisocial Network</title>
+    <use xlinkHref='#logo--icon' />
+  </svg>
 );
 
 export default Logo;
